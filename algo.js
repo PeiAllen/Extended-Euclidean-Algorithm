714c1c7c829b863b865f28262331b33c50bad95b
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2018-01-02 17:36:21 -0500

    add

--- algo.js
+++ algo.js
@@ -18,6 +18,52 @@ function go(){
 
 
 }
+function Dio(){
+    var a=document.getElementById("equation").innerHTML;
+    var a1=a.split("=");
+    var a2;
+    if(a1[0].includes("-")){
+        a2=a1[0].split("-");
+    }
+    else{
+        a2=a1[0].split("+");
+    }
+    var b=parseInt(a2[0].substring(0,a2[0].length-1));
+    var c=parseInt(a2[1].substring(0,a2[1].length-1));
+    var temp1=new Array(4);
+        if(b>c){
+        temp1 =Euclidean(c,b);
+        }
+        else{
+        temp1 =Euclidean(b,c);
+        }
+    if(Math.abs(temp1[0])==Math.abs(b)||Math.abs(temp1[0])==c){
+        if(Math.abs(temp1[2])==Math.abs(b)||Math.abs(temp1[2])==c){
+        temp1[1]*=parseInt(a1[1])/gcf;
+        temp1[3]*=parseInt(a1[1])/gcf;
+        }
+        else{
+        temp1[1]*=parseInt(a1[1])/gcf;
+        temp1[2]*=parseInt(a1[1])/gcf;   
+        }
+    }
+    else{
+    if(Math.abs(temp1[2])==Math.abs(b)||Math.abs(temp1[2])==c){
+        temp1[0]*=parseInt(a1[1])/gcf;
+        temp1[3]*=parseInt(a1[1])/gcf;
+        }
+        else{
+        temp1[0]*=parseInt(a1[1])/gcf;
+        temp1[2]*=parseInt(a1[1])/gcf;   
+        }     
+    }
+    if(temp1[2]<0){
+        document.getElementById("solution").innerHTML=(temp1[0]+"*"+temp1[1]+""+temp1[2]+"*"+temp1[3]+"="+a1[1]);
+        }
+        else{
+        document.getElementById("solution").innerHTML=(temp1[0]+"*"+temp1[1]+"+"+temp1[2]+"*"+temp1[3]+"="+a1[1]);
+        }
+}
 var formulas = [];
     var gcf;
     function Euclidean(a, b){
